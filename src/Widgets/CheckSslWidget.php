61bb92f958d855bafc3a71a1bbe2d9342a6a03b2
joaopaulolndev <joaopaulolndev@users.noreply.github.com>
2024-06-27 16:03:11 +0000

    Fix styling

--- src/Widgets/CheckSslWidget.php
+++ src/Widgets/CheckSslWidget.php
@@ -61,7 +61,7 @@ class CheckSslWidget extends Widget
 
     private static function isValidDomain($domain): bool
     {
-        return (bool)preg_match('/^(?:[a-z0-9](?:[a-z0-9-æøå]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/isu', $domain);
+        return (bool) preg_match('/^(?:[a-z0-9](?:[a-z0-9-æøå]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/isu', $domain);
     }
 
     private function getFaviconByDomain(string $domain): ?string
