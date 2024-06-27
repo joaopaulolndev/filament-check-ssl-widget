5c762116cd90e25d6344fb2cb5609f493fdf484f
Jèfferson Gonçalves <gerson.simao.92@gmail.com>
2024-06-27 09:36:41 -0300

    Adicionando cache e verificação se existe favicon.

--- src/Widgets/CheckSslWidget.php
+++ src/Widgets/CheckSslWidget.php
@@ -34,21 +34,24 @@ class CheckSslWidget extends Widget
         foreach ($domains as $domain) {
             $certificate = SslCertificate::createForHostName($domain);
 
-            if (Str::contains($domain, ['http://', 'https://'])) {
-                $favicon = Favicon::fetch($domain)->getFaviconUrl();
-            } else {
-                $favicon = Favicon::fetch('https://' . $domain)->getFaviconUrl();
-            }
-
             $this->certificates[] = [
                 'domain' => $domain,
                 'issuer' => $certificate->getIssuer(),
                 'is_valid' => $certificate->isValid(),
                 'expiration_date' => $certificate->expirationDate()->diffForHumans(),
                 'expiration_date_in_days' => $certificate->expirationDate()->diffInDays(),
-                'favicon' => $favicon,
+                'favicon' => $this->getFaviconByDomain($domain),
             ];
         }
+    }
+
+    private function getFaviconByDomain(string $domain): ?string
+    {
+        if (! Str::contains($domain, ['http://', 'https://'])) {
+            $domain = 'https://' . $domain;
+        }
+
+        return Favicon::fetch($domain)?->cache(now()->addDay())->getFaviconUrl() ?? null;
     }
 
     public function shouldShowTitle(): bool
